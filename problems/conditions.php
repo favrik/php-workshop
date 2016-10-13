3dd11cd223695e8925f8e37541fdb102c049042c
Favio Manriquez Leon <favio@favrik.com>
2016-10-13 12:27:34 -0700

    Add acute chars

--- problems/conditions.php
+++ problems/conditions.php
@@ -1,25 +1,28 @@
 <?php
 /**
- * Vamos a hacer un algoritmo para determinar si una persona gano la loteria o no.
+ * Vamos a hacer un algoritmo para determinar si una persona ganó la
+ * lotería o no.
  *
- * Definir 3 variables para guardar la siguiente informacion:
+ * Definir 3 variables para guardar la siguiente información:
  * 1. Tiene boleto
- * 2. Numero
- * 3. Comprobante de compra
+ * 2. Número
+ * 3. Tiene Comprobante de compra
  *
- * Si tiene el boleto y el numero es el ganador entonces gano la loteria
+ * Si tiene el boleto y el número es igual al numero ganador entonces ganó la
+ * lotería.
  *
- * Si no tiene boleto pero tiene el comprobante, y el numero es el ganador,
- * entonces tambien gano la loteria.
+ * Si no tiene boleto pero tiene el comprobante, y el número es el ganador,
+ * entonces también ganó la lotería.
  *
- * De cualquier otra forma, no gano la loteria.
+ * De cualquier otra forma, no ganó la lotería.
  *
  *
  * Utilizando las variables predefinidas ($monto y $nombre)
  *
- * Si gano la loteria, imprimir: "Felicidades <nombre>, has ganado <monto>";
+ * Si ganó la lotería, imprimir: "Felicidades <nombre>, has ganado <monto>";
  *
- * Si no gano la loteria, imprimir: "Lo sentimos <nombre>, no has ganado esta vez, intenta de nuevo"
+ * Si no ganó la lotería, imprimir:
+ * "Lo sentimos <nombre>, no has ganado esta vez, intenta de nuevo"
  */
 
 $numero_ganador = 12345;

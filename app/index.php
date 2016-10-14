d93b59146018682db3c3185cfbc3f4bc76f396e3
Favio Manriquez Leon <favio@favrik.com>
2016-10-14 00:49:48 -0700

    Add base style for app

--- app/index.php
+++ app/index.php
@@ -1,32 +1,5 @@
 <?php
-  $title = 'Mis Actividades';
-?>
-<html>
-  <head>
-    <title><?php echo $title; ?></title>
-  </head>
-  <body>
-    <h1><?php echo $title; ?></h1>
-    <form method="post" action="">
-      <label for="thing">Qué quieres hacer?</label>
-      <input type="text" name="thing" id="thing" />
-      <input type="submit" value="Agregar" />
-    </form>
-    <hr />
 
-    <h2></h2>
-    <ul>
-      <li>Hacer tarea de programación</li>
-      <li>Estudiar sobre funciones</li>
-      <li><s>Ir al mandado</s></li>
-      <li>Cita con el dentista a las 3 pm</li>
-    </ul>
-    <hr />
+$title = 'Mis Actividades';
 
-    <p>
-      <strong>Total:</strong> 4  &nbsp;&nbsp;&nbsp;
-      <strong>Completadas:</strong> 1 &nbsp;&nbsp;&nbsp;
-      <strong>Por completar:</strong> 3
-    </p>
-  </body>
-</html>
+require 'index.view.php';
--- app/index.view.php
+++ app/index.view.php
@@ -1,1 +1,51 @@
-// view
+<html>
+  <head>
+    <title><?php echo $title; ?></title>
+    <link href="https://fonts.googleapis.com/css?family=Bungee+Inline|Roboto" rel="stylesheet">
+    <link rel="stylesheet" type="text/css" href="Napp.css?<?php echo time(); ?>">
+  </head>
+  <body>
+
+    <div class="container">
+      <h1><?php echo $title; ?></h1>
+      <form method="post" action="">
+        <div class="stats">
+          <span><strong>Total:</strong> 4</span>
+          <span><strong>Completadas:</strong> 1</span>
+          <span><strong>Por completar:</strong> 3</span>
+        </div>
+        <label for="thing">¿Qué quieres hacer?</label>
+        <input type="text" name="thing" id="thing" />
+        <input type="submit" value="Agregar" />
+      </form>
+
+      <table>
+        <thead>
+          <tr>
+            <th>Id</th>
+            <th>Actividad</th>
+            <th>Completada</th>
+            <th>Opciones</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>1</td>
+            <td>Hacer tarea de programación</td>
+            <td>Si</td>
+            <td><a href="#">Editar</a></td>
+          </tr>
+
+          <tr>
+            <td>1</td>
+            <td>Hacer tarea de programación</td>
+            <td>Si</td>
+            <td><a href="#">Editar</a></td>
+          </tr>
+        </tbody>
+      </table>
+
+    </div>
+  </body>
+</html>
+
